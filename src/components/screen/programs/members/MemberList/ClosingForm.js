c371f07523254663b06042abc98de8ad794c3086
Lalit Kumar <kumar932687@gmail.com>
2026-04-28 21:44:22 +0530

    Updated code

--- src/components/screen/programs/members/MemberList/ClosingForm.js
+++ src/components/screen/programs/members/MemberList/ClosingForm.js
@@ -838,7 +838,7 @@ const ClosingForm = ({ open, onClose, memberData, user, selectedProgram, onSucce
                   format="DD-MM-YYYY"
                   style={{ width: '100%' }}
                   placeholder="Select closing date"
-                  disabledDate={disableMarriageDate}
+                  // disabledDate={disableMarriageDate}
                 />
               </Form.Item>
 
